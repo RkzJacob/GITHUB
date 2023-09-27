1746ec7151ae9c08bd2f65658c4d5a51f628dc39
TuwuS <tomasenri2000@gmail.com>
2023-09-27 17:32:13 -0300

    Kpis Plagas

--- bigeo-duoc/models/formPlague/formplague.service.ts
+++ bigeo-duoc/models/formPlague/formplague.service.ts
@@ -10,9 +10,34 @@ import { Sequelize } from 'sequelize-typescript';
 export class FormPlagueService {
     constructor(
         @InjectModel(formPlague)
-        private readonly plagueModel: typeof formPlague, private readonly sequelize: Sequelize){
-            
-        }
+        private readonly plagueModel: typeof formPlague, private readonly sequelize: Sequelize) {
 
+    }
+    async CantidadPlagasPorSector(): Promise<any> {
+        const query = `
+        SELECT 
+        sector, COUNT(plague) as cant_plaga
+        FROM public.form
+        JOIN public.properties pr on propid="propertiesPropid"
+        LEFT join public."formPlague" pl on plid="formPlaguePlid"
+        GROUP BY sector
+        ORDER BY 2 DESC
+            `;
+        const result = await this.sequelize.query(query, { type: 'SELECT' });
+        return result;
+    }
+    async ConteoPlagas(): Promise<any> {
+        const query = `
+        SELECT 
+        sector,plague as plaga,
+        level as nivel, population, observation
+        FROM public.form
+        JOIN public.properties pr on propid="propertiesPropid"
+        join public."formPlague" pl on plid="formPlaguePlid"
+        ORDER BY 2 
+            `;
+        const result = await this.sequelize.query(query, { type: 'SELECT' });
+        return result;
+    }
 
 }
